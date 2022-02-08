eb7665d1901867b2726159f63f25e598ae93acab
jorgebaratech <93922396+jorgebaratech@users.noreply.github.com>
2022-02-08 13:44:29 +0100

    Update EquipoClasificacion.java

--- EquipoClasificacion.java
+++ EquipoClasificacion.java
@@ -1,14 +1,57 @@
 
 public class EquipoClasificacion {
-	
-	private Equipo equipo;
-	
-	
+	Equipo equipo;
+	Clasificacion clasificacion;
+	private int victorias, derrotas, empates, sumargoles,restargoles, diferenciaEquipo;
 	public Equipo getEquipo() {
 		return equipo;
 	}
 	public void setEquipo(Equipo equipo) {
 		this.equipo = equipo;
+	}
+	public Clasificacion getClasificacion() {
+		return clasificacion;
+	}
+	public void setClasificacion(Clasificacion clasificacion) {
+		this.clasificacion = clasificacion;
+	}
+	public int getVictorias() {
+		return victorias;
+	}
+	public void setVictorias(int victorias) {
+		this.victorias = victorias;
+	}
+	public int getDerrotas() {
+		return derrotas;
+	}
+	public void setDerrotas(int derrotas) {
+		this.derrotas = derrotas;
+	}
+	public int getEmpates() {
+		return empates;
+	}
+	public void setEmpates(int empates) {
+		this.empates = empates;
+	}
+	
+	
+	public int getDiferenciaEquipo() {
+		return diferenciaEquipo;
+	}
+	public void setDiferenciaEquipo(int diferenciaEquipo) {
+		this.diferenciaEquipo = diferenciaEquipo;
+	}
+	public int getSumargoles() {
+		return sumargoles;
+	}
+	public void setSumargoles(int sumargoles) {
+		this.sumargoles = sumargoles;
+	}
+	public int getRestargoles() {
+		return restargoles;
+	}
+	public void setRestargoles(int restargoles) {
+		this.restargoles = restargoles;
 	}
 	
 	
